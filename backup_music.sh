c0c919babea8986fd046f82d2c840edd4310182a
Imad Saddik <simad3647@gmail.com>
2026-02-01 11:11:00 +0100

    refactor: remove logging from bash script and put it in the cron file

--- backup_music.sh
+++ backup_music.sh
@@ -4,15 +4,7 @@ PROJECT_DIR="$HOME/Projects/BackUpYouTubeMusic"
 MUSIC_DIR="$HOME/Music/YouTubeMusic/no_lyrics"
 PLAYLIST_URL="https://www.youtube.com/playlist?list=PL22-qG2MGGhAr96FUEz9jv11sVx0nSZTY"
 
-LOG_DIR="$PROJECT_DIR/logs"
-mkdir -p "$LOG_DIR"
 mkdir -p "$MUSIC_DIR"
-
-# Redirect all stdout and stderr to the log file
-LOG_FILE="$LOG_DIR/weekly_backup_$(date +%Y-%m-%d).log"
-exec >> "$LOG_FILE" 2>&1
-
-echo "--- Starting weekly backup at $(date) ---"
 
 if [ ! -d "$PROJECT_DIR" ]; then
     echo "Error: Directory $PROJECT_DIR does not exist."
@@ -30,16 +22,13 @@ fi
 
 cd "$PROJECT_DIR" || exit 1
 
-echo "Running yt-dlp backup"
-
+echo "Updating yt-dlp"
 pip install --upgrade yt-dlp
 
+echo "Running download"
 yt-dlp -x --audio-format mp3 \
   --download-archive "$PROJECT_DIR/archive.txt" \
   --downloader aria2c \
   --downloader-args "aria2c:-x 16 -s 16 -k 1M" \
   -o "$MUSIC_DIR/%(title)s.%(ext)s" \
   "$PLAYLIST_URL"
-
-echo "--- Backup job finished at $(date) ---"
-exit 0
